fac5dd3d190a6f15daaefa71bada5a043cd905ba
bbaktaeho <lth9036@gmail.com>
2020-04-30 11:16:49 +0900

    [modify] 서버 시작 메서드 추가와 비동기 처리되는 로직을 수정함

--- backEnd/src/app.ts
+++ backEnd/src/app.ts
@@ -1,56 +1,93 @@
-import * as express from "express";
-import * as morgan from "morgan";
-import * as hpp from "hpp";
-import * as helmet from "helmet";
-import routes from "./api/routes";
-import config from "./configs";
-import logger from "./utils/logger";
+import * as express from 'express'
+import { Application } from 'express'
+import * as morgan from 'morgan'
+import * as hpp from 'hpp'
+import * as helmet from 'helmet'
+import * as bodyParser from 'body-parser'
+import { sequelize } from './models'
+import routes from './api/routes'
+import config from './configs'
+import logger from './utils/logger'
 
 class App {
-  private app: express.Application;
+  private app: express.Application
 
-  constructor(port: number) {
-    this.app = express();
-    (async () => {
+  constructor() {
+    this.app = express()
+    ;(async () => {
       try {
-        await this.setting(); // 익스프레스 셋팅
-        await this.loader(); // 모듈 로딩
-        await this.router(); // 라우터 셋팅
-        await this.listen(); // 서버 시작
+        this.start()
       } catch (error) {
-        logger.error(error.message);
+        logger.error(error.message)
       }
-    })();
-  }
-  private async setting() {
-    this.app.set("port", config.port);
+    })()
   }
 
-  private async loader() {
-    this.app.use(hpp());
-    this.app.use(helmet());
-    this.app.use(morgan("dev"));
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
+  /**
+   * * 익스프레스 셋팅
+   * TODO: 정적 파일 제공과 서비스
+   * TODO: 템플릿 엔진 셋팅
+   * TODO: 값 셋팅
+   */
+  private async setting({ expressApp }: { expressApp: Application }): Promise<void> {
+    expressApp.set('port', config.port)
+    // expressApp.static()
+    // expressApp.engine() ...
   }
 
-  private async router() {
-    this.app.use("/api", routes()); // 라우터 등록
-    this.app.get("/favicon.ico", (req, res) => {
-      // favicon.ico throw err 막기
-      res.status(204);
-    });
-    this.app.use((req, res, next) => {
-      let err = new Error("Not found");
-      next(err);
-    });
+  /**
+   * * 외부 모듈과 최상위 미들웨어 로더
+   */
+  private async loader({ expressApp }: { expressApp: Application }): Promise<void> {
+    await sequelize
+      .sync({ force: false })
+      .then(() => {
+        logger.info('db connected')
+      })
+      .catch(() => {
+        logger.error('db error')
+      })
+    expressApp.use(hpp())
+    expressApp.use(helmet())
+    expressApp.use(morgan('dev'))
+    expressApp.use(express.json())
+    expressApp.use(express.urlencoded({ extended: true }))
+    expressApp.use(bodyParser.json())
+    expressApp.use(bodyParser.urlencoded({ extended: true }))
   }
 
-  private async listen() {
-    this.app.listen(this.app.get("port"), () => {
-      logger.info(`App listening on the port ${this.app.get("port")}`);
-    });
+  /**
+   * * 라우팅 설정
+   */
+  private async router({ expressApp }: { expressApp: Application }): Promise<void> {
+    expressApp.use('/api', routes())
+    expressApp.get('/favicon.ico', (req, res) => {
+      res.status(204)
+    })
+    expressApp.use((req, res, next) => {
+      let err = new Error('Not found')
+      next(err)
+    })
+  }
+
+  /**
+   * * 포트 연결을 바인딤
+   */
+  private async listen({ expressApp }: { expressApp: Application }): Promise<void> {
+    expressApp.listen(expressApp.get('port'), () => {
+      logger.info(`App listening on the port ${expressApp.get('port')}`)
+    })
+  }
+
+  /**
+   * * 서버 시작
+   */
+  public async start(): Promise<void> {
+    await this.setting({ expressApp: this.app })
+    await this.loader({ expressApp: this.app })
+    await this.router({ expressApp: this.app })
+    await this.listen({ expressApp: this.app })
   }
 }
 
-export default new App(9000);
+export default new App()
